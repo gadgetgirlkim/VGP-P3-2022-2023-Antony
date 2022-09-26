cc768cffbd5f73a6c6f80b9d67d7c4955955c736
antonycode17 <148118@student.vansd.org>
2022-09-26 10:22:51 -0700

    can move car with wasd

--- CreateWithCode/Prototype1/Assets/Scripts/PlayerController.cs
+++ CreateWithCode/Prototype1/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
     public float turnSpeed;
+    public float horizontalInput;
+    public float fowardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -16,8 +18,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        fowardInput = Input.GetAxis("Vertical");
+
         //move the vehicale foward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed); 
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);   
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * fowardInput); 
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);   
     }
 }
