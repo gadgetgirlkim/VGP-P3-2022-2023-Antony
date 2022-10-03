f15ba4c2c9a9acb1ee5d2790fbbab7c78de9a9df
antonycode17 <148118@student.vansd.org>
2022-10-03 10:21:40 -0700

    made plane go forward

--- CreateWithCode/Prototype1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ CreateWithCode/Prototype1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public float speed;
-    public float rotationSpeed;
-    public float verticalInput;
-
+    public float speed = 20f;
+    public float rotationSpeed = 0f;
+    public float verticalInput = 20f;
+    private float turnSpeed = 60.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,9 +21,9 @@ public class PlayerControllerX : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
 
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.back * speed);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.right, turnSpeed * verticalInput * Time.deltaTime);
     }
 }
