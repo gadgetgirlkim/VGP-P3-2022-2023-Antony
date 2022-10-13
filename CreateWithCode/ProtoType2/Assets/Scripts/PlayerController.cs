f288d714f5ca8d123d51be7b8194b1ba3640a38c
antonycode17 <148118@student.vansd.org>
2022-10-13 10:47:02 -0700

    Made prefabs

--- CreateWithCode/ProtoType2/Assets/Scripts/PlayerController.cs
+++ CreateWithCode/ProtoType2/Assets/Scripts/PlayerController.cs
@@ -33,9 +33,10 @@ public class PlayerController : MonoBehaviour
         
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
-    }
+    
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
+    }
 }
