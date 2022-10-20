aba10925f575f2805030c72ce49bc8be59ce4dde
antonycode17 <148118@student.vansd.org>
2022-10-20 09:26:12 -0700

    fixed spawning

--- CreateWithCode/ProtoType2/Assets/Scripts/SpawnManager.cs
+++ CreateWithCode/ProtoType2/Assets/Scripts/SpawnManager.cs
@@ -15,8 +15,9 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.S)){}
+        if(Input.GetKeyDown(KeyCode.S)){
             int animalIndex = Random.Range(0, animalPrefabs.Length);
             Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
+        }
     }
 }
