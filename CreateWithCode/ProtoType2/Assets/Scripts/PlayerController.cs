c7085e990c6c2b71b75897613f9b785bd3705a9f
antonycode17 <148118@student.vansd.org>
2022-10-31 10:19:46 -0700

    imported challenge 2

--- CreateWithCode/ProtoType2/Assets/Scripts/PlayerController.cs
+++ CreateWithCode/ProtoType2/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
-    public float xRange = 40;
+    public float xRange = 10.0f;
 
     public GameObject projectilePrefab;
 
