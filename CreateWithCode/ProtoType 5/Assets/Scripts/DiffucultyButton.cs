9a32ceecec7d908a69b90d136b5c1c06417fd249
antonycode17 <148118@student.vansd.org>
2023-03-22 10:42:16 -0700

    finished prototype 5

--- CreateWithCode/ProtoType 5/Assets/Scripts/DiffucultyButton.cs
+++ CreateWithCode/ProtoType 5/Assets/Scripts/DiffucultyButton.cs
@@ -6,13 +6,15 @@ using UnityEngine.UI;
 public class DiffucultyButton : MonoBehaviour
 {
     private Button button;
-
+    private GameManager gameManager;
+    public int diffuculty;
 
     // Start is called before the first frame update
     void Start()
     {
         button = GetComponent<Button>();
         button.onClick.AddListener(SetDiffuculty);
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -24,5 +26,7 @@ public class DiffucultyButton : MonoBehaviour
     void SetDiffuculty()
     {
         Debug.Log(gameObject.name + " was clicked");
+        gameManager.StartGame(diffuculty);
+
     }
 }
--- CreateWithCode/ProtoType 5/Assets/Scripts/GameManager.cs
+++ CreateWithCode/ProtoType 5/Assets/Scripts/GameManager.cs
@@ -14,14 +14,12 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI gameOverText;
     public bool isGameActive;
     public Button restartButton;
+    public GameObject titleScreen;
 
     // Start is called before the first frame update
     void Start()
     {
-       isGameActive = true;
-       score = 0;
-       StartCoroutine(SpawnTarget()); 
-       UpdateScore(0);
+      
        
     }
 
@@ -61,4 +59,15 @@ public class GameManager : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
+
+    public void StartGame(int diffuculty)
+    {
+       spawnRate /= diffuculty;
+       isGameActive = true;
+       score = 0;
+       StartCoroutine(SpawnTarget()); 
+       UpdateScore(0);
+       titleScreen.gameObject.SetActive(false);
+    }
+
 }
