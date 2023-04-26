06bcced7f3f490b252b76c60c880749ce84b40f3
antonycode17 <148118@student.vansd.org>
2023-04-26 10:44:56 -0700

    added movement code to personnel project

--- CreateWithCode/PersonalProject/Assets/Scripts/PlayerController.cs
+++ CreateWithCode/PersonalProject/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float speed = 10.0f;
+    private Rigidbody playerRb;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+      playerRb = GetComponent<Rigidbody>();  
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+
+        playerRb.AddForce(Vector3.forward * speed * verticalInput);
+        playerRb.AddForce(Vector3.right * speed * horizontalInput);
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Player has collided with the Enemy");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PowerUp"))
+        {
+            Destroy(other.gameObject);
+        }
     }
 }
---
+++ CreateWithCode/PersonalProject/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] enemies;
+    public GameObject PowerUp;
+
+    private float zEnemySpawn = 12.0f;
+    private float xSpawnRange = 16.0f;
+    private float zPowerupRange = 5.0f;
+    private float ySpawn = 0.75f;
+
+    private float powerupSpawnTime = 5.0f;
+    private float enemySPawnTime = 1.0f;
+    private float
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
